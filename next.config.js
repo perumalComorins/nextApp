d04bf79871c65fd080a62a13b470f37a340c8c29
perumalComorins <86822193+perumalComorins@users.noreply.github.com>
2022-01-10 14:09:44 +0530

    Second Commit for EsLint
    Second Commit for EsLint

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
 module.exports = {
     reactStrictMode: true,
+    eslint: {
+        // Warning: This allows production builds to successfully complete even if
+        // your project has ESLint errors.
+        ignoreDuringBuilds: true,
+    },
     serverRuntimeConfig: {
         secret: 'THIS IS USED TO SIGN AND VERIFY JWT TOKENS, REPLACE IT WITH YOUR OWN SECRET, IT CAN BE ANY STRING'
     },
